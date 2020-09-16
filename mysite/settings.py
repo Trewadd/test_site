f900dc4917b6bd02e17a66aa2e9793366eee1dfd
Volodymyr <iaremenko.volodymyr@gmail.com>
2020-09-16 14:34:20 +0300

    Update Allowed_hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 # DEBUG = False
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
